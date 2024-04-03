d8b5fa7ed0a6d1286b281fcbbf5ef38d90436cae
[Tuba Asif] <[tubaasif146@gmail.com]>
2024-04-03 16:25:36 -0700

    first commit

---
+++ hellowworld.js
@@ -0,0 +1,11 @@
+//Hellow world 
+console.log("Hellow from Tuba");
+//Addition
+console.log(5 + 2); //7
+//Variable
+var userName = "Tuba";
+console.log(userName); //Tuba
+//Another example of variable
+var num1 = 3;
+var num2 = 2;
+console.log(num1 + num2);
---
+++ hellowworld.ts
@@ -0,0 +1,16 @@
+//Hellow world 
+console.log("Hellow from Tuba")
+
+//Addition
+console.log(5+2) //7
+
+//Variable
+let userName ="Tuba"
+console.log(userName) //Tuba
+
+//Another example of variable
+let num1 = 3
+let num2 = 2
+console.log(num1 + num2) //5
+
+
